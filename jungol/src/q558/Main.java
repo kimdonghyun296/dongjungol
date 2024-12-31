8c35edec3a9c3bb865881318144286405deac5e6
2q3w4 <2q3w4@TABLET-7DI70QFI>
2024-12-31 17:28:14 +0900

    q558

---
+++ jungol/src/q558/Main.java
@@ -0,0 +1,31 @@
+package q558;
+
+/*import java.util.*;
+public class Main {
+	public static void main(String[] args)
+	{
+		int idx=0;
+		Scanner sc=new Scanner(System.in);
+		int[] abc=new int[100];
+		for(;;)
+		{
+			abc[i]=sc.nextInt();
+			if(abc[i]==0)
+			{
+				sc.close();
+				break;
+			}
+			abc[i++]=num1;
+		}
+		for()
+		{
+			System.out.println(abc[i]);
+		}
+	}
+}
+*/
+public class Main {
+    public static void main(String[] args) {
+      
+    }
+}
