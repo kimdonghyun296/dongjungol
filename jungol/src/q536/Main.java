17b5366ef0f159998ae4799940de3b14b9bef807
2q3w4 <2q3w4@TABLET-7DI70QFI>
2024-12-31 12:43:32 +0900

    q536

---
+++ jungol/src/q536/Main.java
@@ -0,0 +1,13 @@
+package q536;
+
+public class Main {
+	public static void main(String[] args) 
+	{
+		int i=1;
+        while(i<16)
+        {
+            System.out.printf("%d ",i);
+            i++;
+        }
+	}
+}
