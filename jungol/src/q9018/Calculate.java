5a3ee105fd554966a762c4a8d8f9dbf944614511
2q3w4 <2q3w4@TABLET-7DI70QFI>
2025-01-06 17:40:10 +0900

    q9018

---
+++ jungol/src/q9018/Calculate.java
@@ -0,0 +1,18 @@
+package q9018;
+
+public class Calculate {
+	public int result1;
+	public int result2;
+	public int result3;
+	public int result4;
+	public int result5;
+	Calculate(int num1,int num2)
+	{
+		this.result1=num1+num2;
+		this.result2=num1-num2;
+		this.result3=num1*num2;
+		this.result4=num1/num2;
+		this.result5=num1%num2;
+	}
+
+}
