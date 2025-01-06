d8040132baa25187b119340180d50f6e7f1bb73d
2q3w4 <2q3w4@TABLET-7DI70QFI>
2025-01-06 17:42:36 +0900

    q571

---
+++ jungol/src/q571/Main.java
@@ -0,0 +1,26 @@
+package q571;
+
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		Scanner sc =new Scanner(System.in);
+		
+		int num2=sc.nextInt();
+		
+		print(num2);
+		
+	}
+	
+	
+	public static void print(int num1)
+	{
+		for(int i=0;i<num1;i++)
+		{
+			System.out.println("~!@#$^&*()_+|");
+		}
+	}
+
+}
