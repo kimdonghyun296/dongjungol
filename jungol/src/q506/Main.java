1fb2ed3efcc422e8485d454cb451d4f5487cecff
2q3w4 <2q3w4@TABLET-7DI70QFI>
2025-01-06 17:41:24 +0900

    q506

---
+++ jungol/src/q506/Main.java
@@ -0,0 +1,12 @@
+package q506;
+
+public class Main {
+	public static void main(String[] args)
+	{
+        System.out.println("My height");
+        System.out.printf("%d\n",170);
+        System.out.println("My weight");
+        System.out.printf("%.6f",68.6);
+    }
+
+}
