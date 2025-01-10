5c7bb30d7f69af32c393bee21fcb5957f34e1e9f
2q3w4 <2q3w4@192.168.219.106>
2025-01-10 20:24:48 +0900

    q514

---
+++ jungol/src/q514/Main.java
@@ -0,0 +1,15 @@
+package q514;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("height = ");
+		int num1=sc.nextInt();
+		sc.close();
+		
+		System.out.println("Your height is "+num1+"cm.");
+	}
+}
